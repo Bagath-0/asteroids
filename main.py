81d01253e8b7ce6027e0d1df15117f478d7b4186
Bagath-0 <michaelsta@hotmail.co.uk>
2025-01-26 18:06:59 +0000

    added FPS limiter

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ def main():
     print ("Starting asteroids!")   
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
-    while True == True:
+    clock = pygame.time.Clock()
+    FPS = 60
+    dt = 0
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         color = (0,0,0)
         screen.fill(color)
         pygame.display.flip()
-
+        dt = clock.tick(FPS)
+        dt = dt/1000
+        
 
 
 
