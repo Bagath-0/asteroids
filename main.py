a150c73ce84cf33ce95bfa96dfcc0ce5fd2e3c70
Bagath-0 <michaelsta@hotmail.co.uk>
2025-01-26 18:44:35 +0000

    player model added

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 pygame.init
 from constants import *
+from player import *
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
     print ("Starting asteroids!")   
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     clock = pygame.time.Clock()
     FPS = 60
     dt = 0
@@ -13,8 +16,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         color = (0,0,0)
         screen.fill(color)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(FPS)
         dt = dt/1000
