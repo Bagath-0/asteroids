0c5a1e2aac7d7d1b1a325d1beeca0a501643267d
Bagath-0 <michaelsta@hotmail.co.uk>
2025-01-26 19:45:41 +0000

    groups added to loop

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     clock = pygame.time.Clock()
     FPS = 60
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    print (updatable)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -19,8 +24,10 @@ def main():
         
         color = (0,0,0)
         screen.fill(color)
-        player.update(dt)
-        player.draw(screen)
+        for entry in updatable:
+            entry.update(dt)
+        for entry in drawable:
+            entry.draw(screen)
         pygame.display.flip()
         dt = clock.tick(FPS)
         dt = dt/1000
